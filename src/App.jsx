78956fdd98d4b8f2d5422985776893ea6c4357ed
Muhammad Bilal <bilaltheofficial@gmail.com>
2025-08-16 15:28:18 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TradingViewWidget from './components/tradingView'
+
+function App() {
+
+  return (
+    <>
+      <div style={{ height: "100vh", width: "205vh" }}>
+        <TradingViewWidget />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/tradingView.jsx
@@ -0,0 +1,47 @@
+import { memo, useEffect, useRef } from "react";
+
+function TradingViewWidget() {
+  const container = useRef();
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
+      {
+        "allow_symbol_change": false,
+        "calendar": false,
+        "details": false,
+        "hide_side_toolbar": true,
+        "hide_top_toolbar": false,
+        "hide_legend": false,
+        "hide_volume": false,
+        "hotlist": false,
+        "interval": "D",
+        "locale": "en",
+        "save_image": true,
+        "style": "9",
+        "symbol": "OANDA:XAUUSD",
+        "theme": "dark",
+        "timezone": "Etc/UTC",
+        "backgroundColor": "#0F0F0F",
+        "gridColor": "rgba(242, 242, 242, 0.06)",
+        "watchlist": [],
+        "withdateranges": false,
+        "compareSymbols": [],
+        "studies": [],
+        "autosize": true
+      }`;
+    container.current.appendChild(script);
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container" ref={container} style={{ height: "800px", width: "100%", minWidth: "600px" }}>
+      <div className="tradingview-widget-container__widget" style={{ height: "100%", width: "100%" }}></div>
+      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/OANDA:XAUUSD/" rel="noopener nofollow" target="_blank"><span className="blue-text">XAUUSD quotes by TradingView</span></a></div>
+    </div>
+  );
+}
+
+export default memo(TradingViewWidget);
